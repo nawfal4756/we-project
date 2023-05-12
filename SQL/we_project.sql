1cbdb22b9ebbe6b1afa898679890b098e632978e
Muhammad Nawfal Mehboob <nawfalmehboob@gmail.com>
2023-05-13 02:46:38 +0500

    Comapny Logo field added

--- SQL/we_project.sql
+++ SQL/we_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3310
--- Generation Time: May 12, 2023 at 11:20 PM
+-- Generation Time: May 12, 2023 at 11:45 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -46,6 +46,7 @@ CREATE TABLE `admin` (
 CREATE TABLE `company` (
   `id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
+  `logo` varchar(250) NOT NULL,
   `location` varchar(300) NOT NULL,
   `blocked` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
