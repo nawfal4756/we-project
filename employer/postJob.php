c9d2d5f40b4404350d3e0e7fbf6e3c0f2a875a5d
Muhammad Nawfal Mehboob <nawfalmehboob@gmail.com>
2023-05-14 22:21:07 +0500

    postJob ajax issue fixed

--- employer/postJob.php
+++ employer/postJob.php
@@ -6,15 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link href="../styles/styles.css" rel="stylesheet">
+    <link href="/styles/styles.css" rel="stylesheet">
     <link href="../styles/employer.css" rel="stylesheet">
-    <script src="bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.js"></script>
+    <script src="/bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/core.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/md5.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body class="bg-secondary">
@@ -27,6 +24,9 @@
     <div class="container card-0 justify-content-center ">
   <div class="card-body px-sm-4 px-0">
     <div class="row justify-content-center mb-5">
+      <div class="col-md-10">
+        <?php require "../Components/alert.php"; ?>
+      </div>
       <div class="col-md-10 col">
         <h3 class="font-weight-bold ml-md-0 mx-auto text-center text-sm-left"> Post a Job </h3>
       </div>
